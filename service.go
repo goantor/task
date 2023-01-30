3af2ba20d69e3332ad711d16752d64d6e2da392d
leaf_go <leaf_go@126.com>
2023-01-30 13:19:08 +0800

    x

--- service.go
+++ service.go
@@ -1,6 +1,10 @@
 package task
 
-import "github.com/goantor/deamon"
+import (
+	"github.com/goantor/deamon"
+	"os"
+	"os/signal"
+)
 
 const (
 	Once = iota + 1
@@ -21,5 +25,9 @@ func (t *Task) Register(handler RegisterHandler) {
 }
 
 func (t *Task) Boot() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
 	deamon.DaemonManager.Start()
+
+	<-c
 }
