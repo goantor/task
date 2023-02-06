a60cf42be854098c36d7a1d5b1a6de6137ce251f
leaf_go <leaf_go@126.com>
2023-02-06 18:35:01 +0800

    x

--- service.go
+++ service.go
@@ -30,7 +30,7 @@ func (t *Task) Boot() error {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
 
-	t.routeHandler(deamon.DaemonManager)
+	t.RouteHandler(deamon.DaemonManager)
 	deamon.DaemonManager.Start()
 
 	<-c
